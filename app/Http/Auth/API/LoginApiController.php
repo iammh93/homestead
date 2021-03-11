cb8821108f7880048321f4c6be397557c8e5307d
Wai Song <waisong.tan@anacle.com>
2021-03-11 18:01:07 +0800

    Added api for login

---
+++ app/Http/Auth/API/LoginApiController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Auth\API;
+
+use Carbon\Carbon;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use App\Http\Auth\Controllers\LoginController;
+
+class LoginApiController extends LoginController
+{
+    public function login(Request $request)
+    {
+        $this->validateLogin($request);
+
+        if (method_exists($this, 'hasTooManyLoginAttempts') &&
+            $this->hasTooManyLoginAttempts($request)) {
+            $this->fireLockoutEvent($request);
+
+            return $this->sendLockoutResponse($request);
+        }
+        
+        if (!$this->attemptLogin($request)) {
+            return response()->json([
+                'message' => 'Unauthorized'
+            ], 401);
+        }
+
+        $user = $request->user();
+        $tokenResult = $user->createToken('Personal Access Token');
+        $token = $tokenResult->token;
+
+        if ($request->remember_me) {
+            $token->expires_at = Carbon::now()->addWeeks(1);
+        }
+
+        $token->save();
+
+        return response()->json([
+            'access_token' => $tokenResult->accessToken,
+            'token_type' => 'Bearer',
+            'expires_at' => Carbon::parse(
+                $tokenResult->token->expires_at
+            )->toDateTimeString()
+        ]);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,9 @@
 <?php
 
 use Illuminate\Http\Request;
+use App\Http\Auth\API as ApiControllers;
 use Illuminate\Support\Facades\Route;
+use App\Http\Auth\Mobile\Controllers as AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,4 +18,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::group(['namespace' => ApiControllers::class], function() {
+    Route::post('login', 'LoginApiController@login');
 });
